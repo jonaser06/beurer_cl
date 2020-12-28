c5d6ece887a6ecd875ac1a29d291b04f12ecf676
renzo <renzoedward23@gmail.com>
2020-12-27 22:24:04 -0500

    add payu log

--- beurer_plantilla/test.php
+++ beurer_plantilla/test.php
@@ -1,15 +1,15 @@
 <?php
 
 $mensajeLog .= print_r($_POST,true) . "\r\n";
-if(strlen($mensajeLog)>0){
-    $filename = "pruebconf.txt";
-    $fp = fopen($filename, "a");
-    if($fp) {
-    fwrite($fp, $mensajeLog, strlen($mensajeLog));
-    fclose($fp);
+    if(strlen($mensajeLog)>0){
+        $filename = "pruebconf.txt";
+        $fp = fopen($filename, "a");
+        if($fp) {
+        fwrite($fp, $mensajeLog, strlen($mensajeLog));
+        fclose($fp);
 
+        }
     }
-}
 
 
 ?>
