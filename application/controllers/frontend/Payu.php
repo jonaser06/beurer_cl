d6a173a3326583fb8bd69dbbf4f05ba8a2f45bb9
renzo <renzoedward23@gmail.com>
2020-12-27 23:59:28 -0500

    add test

--- application/controllers/frontend/Payu.php
+++ application/controllers/frontend/Payu.php
@@ -16,7 +16,7 @@ class Payu extends MY_Controller
         $mensajeLog .= print_r($_POST,true) . "\r\n";
         if(strlen($mensajeLog)>0){
             $filename = "payu_log.txt";
-            $fp = fopen($filename, "a");
+            $fp = @fopen($filename, "a");
             if($fp) {
                 fwrite($fp, $mensajeLog, strlen($mensajeLog));
                 fclose($fp);
